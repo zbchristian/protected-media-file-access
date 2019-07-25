8b368022921ed1ac22c070dd25beb93db5527ef5
Christian Zeitnitz <christian@zeitnitz.eu>
2019-07-25 11:52:43 +0200

    Added webm and mp4 to mime_types_image.php

--- include/mime_types_image.php
+++ include/mime_types_image.php
@@ -5,6 +5,8 @@ define("_MIME_IMG_",array(
 	array("mime" => "image/jpeg", 	"ext" => "jpg"),
 	array("mime" => "image/png", 	"ext" => "png"),
 	array("mime" => "image/svg+xml","ext" => "svg"),
-	array("mime" => "image/tiff", 	"ext" => "tiff")
+	array("mime" => "image/tiff", 	"ext" => "tiff"),
+	array("mime" => "video/webm",   "ext" => "webm"),
+	array("mime" => "video/mp4",    "ext" => "mp4")
 	));
 ?>
